5fadfb33dc42a59c02c130271904838b158eee39
Lucky3028 <yubi@clov3r.cc>
2024-09-23 07:08:26 +0900

    fix: set language attributes in html tag

--- src/index.tsx
+++ src/index.tsx
@@ -9,7 +9,7 @@ app.route("/api", apiRoutes);
 app.get("*", (c) => {
   return c.html(
     renderToString(
-      <html>
+      <html lang="ja">
         <head>
           <meta charSet="utf-8" />
           <meta content="width=device-width, initial-scale=1" name="viewport" />
