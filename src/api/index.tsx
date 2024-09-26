be42210ed335d8165ecc3f1aaebe70895ba47201
Lucky3028 <yubi@clov3r.cc>
2024-09-27 00:11:33 +0900

    feat: use env var for ORIGIN

--- src/api/index.tsx
+++ src/api/index.tsx
@@ -1,17 +1,22 @@
 import { getAuth, oidcAuthMiddleware } from '@hono/oidc-auth';
 import { Hono } from 'hono';
+import { env } from 'hono/adapter';
 import { cors } from 'hono/cors';
 import { csrf } from 'hono/csrf';
 import { secureHeaders } from 'hono/secure-headers';
 import { authApi } from './auth';
 
-const app = new Hono().use(secureHeaders());
+type Env = {
+  FRONTEND_ORIGIN: string;
+};
+
+const app = new Hono<{ Bindings: Env }>().use(secureHeaders());
 
 // CORS
 app.use(async (c, next) => {
+  const { FRONTEND_ORIGIN } = env(c);
   const handler = cors({
-    // TODO: use env
-    origin: 'http://localhost:5173',
+    origin: FRONTEND_ORIGIN,
     // 10 minutes
     maxAge: 60 * 10,
     credentials: true,
@@ -22,9 +27,9 @@ app.use(async (c, next) => {
 
 // CSRF
 app.use(async (c, next) => {
+  const { FRONTEND_ORIGIN } = c.env;
   const handler = csrf({
-    // TODO: use env
-    origin: 'http://localhost:5173',
+    origin: FRONTEND_ORIGIN,
   });
 
   return handler(c, next);
