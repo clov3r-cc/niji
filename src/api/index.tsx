7c02b1fdd502ccf93b3923f222a26dc8fe0d5a7a
Lucky3028 <yubi@clov3r.cc>
2024-09-26 23:38:08 +0900

    feat: add CSRF settings

--- src/api/index.tsx
+++ src/api/index.tsx
@@ -1,14 +1,34 @@
 import { getAuth, oidcAuthMiddleware } from '@hono/oidc-auth';
 import { Hono } from 'hono';
 import { cors } from 'hono/cors';
+import { csrf } from 'hono/csrf';
 import { secureHeaders } from 'hono/secure-headers';
 import { authApi } from './auth';
 
-const app = new Hono().use(
-  secureHeaders(),
-  // TODO: Use env
-  cors({ origin: 'http://localhost:5173' }),
-);
+const app = new Hono().use(secureHeaders());
+
+// CORS
+app.use(async (c, next) => {
+  const handler = cors({
+    // TODO: use env
+    origin: 'http://localhost:5173',
+    // 10 minutes
+    maxAge: 60 * 10,
+    credentials: true,
+  });
+
+  return handler(c, next);
+});
+
+// CSRF
+app.use(async (c, next) => {
+  const handler = csrf({
+    // TODO: use env
+    origin: 'http://localhost:5173',
+  });
+
+  return handler(c, next);
+});
 
 export const apiRoutes = app
   .get('/health', (c) => c.json({ status: 200 }))
